01c5f90c8621b90ed16f7c34c06da34ecd6d0874
= <amidvidy@gmail.com>
2013-12-03 14:03:17 -0800

    output CSV

--- bench.cpp
+++ bench.cpp
@@ -47,49 +47,61 @@ long hammerArray(counter::ConcurrentCounter *counter, int nthreads, int nwrites,
   }
 
   timer::time_point end_time = timer::now();
-  if (isTx) {
-    sync::TransactionalScope::printStats(ts);
-  }
+  //if (isTx) {
+  //sync::TransactionalScope::printStats(ts);
+  //}
   return std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
   
 }
 
 int main(void) {
-  std::size_t num_elements = 128;
 
+  std::size_t max_elements = 4096;
+  
   int nthreads = 8, nwrites = 1000000;
 
-  // Initialize all impls for testing
-  std::map<Impl, counter::ConcurrentCounter*> impls;
+  // Print CSV headers
+  std::cout << "Implementation,NumElements,TimeMillis,ThroughputWPerSec,AvgNanos" << std::endl;
+  std::string DELIM(",");
 
-  impls[NoSync] = new counter::IncorrectConcurrentCounter(num_elements);
-  impls[Coarse] = new counter::CoarseConcurrentCounter(num_elements);
-  impls[Fine] =  new counter::CoarseConcurrentCounter(num_elements);
-  impls[RTMAdaptiveCoarse] =  new counter::RTMCoarseConcurrentCounter(num_elements);
-  impls[RTMAdaptiveFine] =  new counter::RTMFineConcurrentCounter(num_elements);
-  impls[RTMNaive] = new counter::RTMConcurrentCounter(num_elements);
+  for (std::size_t num_elements = 1; num_elements <= max_elements; num_elements *= 2) {
 
-  // run benchmarks on each array and print output
-  for (auto& impl : impls) {
-    bool isRTM = (impl.first >= RTMAdaptiveCoarse) && (impl.first <= RTMAdaptiveFine);
-    auto run_time = hammerArray(impl.second, nthreads, nwrites, isRTM);
-    int total_recorded = impl.second->total();
-    int total_expected = nthreads * nwrites;
+    // Initialize all impls for testing
+    std::map<Impl, counter::ConcurrentCounter*> impls;
 
-    // i should really be using the std::chrono time conversions but this code is low priority
-    auto avg = run_time / (double)total_recorded;
+    //impls[NoSync] =  new counter::IncorrectConcurrentCounter(num_elements);
+    impls[Coarse] = new counter::CoarseConcurrentCounter(num_elements);
+    impls[Fine] =  new counter::CoarseConcurrentCounter(num_elements);
+    impls[RTMAdaptiveCoarse] =  new counter::RTMCoarseConcurrentCounter(num_elements);
+    impls[RTMAdaptiveFine] =  new counter::RTMFineConcurrentCounter(num_elements);
+    impls[RTMNaive] = new counter::RTMConcurrentCounter(num_elements);
 
-    int missing = total_recorded - total_expected;
-    std::cout << "Implementation: " << ImplStr[impl.first] << std::endl;
-    std::cout << "Elapsed Time (ms): " << (double)run_time * 0.001 << std::endl;
-    std::cout << "Avg (ns): " << avg * 1000 << std::endl;
-    std::cout << "Total = " << total_recorded << "\tExpected = " << total_expected << std::endl;
-    std::cout << ((total_recorded == total_expected) ? 
-		  "All writes were recorded." : 
-                     ((missing<0) ?
-		      (std::string("There were ") + std::to_string(-missing) + std::string(" missing writes!")) :
-		      (std::string("There were ") + std::to_string(missing) + std::string(" writes that should NOT have occured. This should never happen.")))) << std::endl;
-    delete impl.second;
+    // run benchmarks on each array and print output
+    for (auto& impl : impls) {
+      bool isRTM = (impl.first >= RTMAdaptiveCoarse) && (impl.first <= RTMAdaptiveFine);
+      auto run_time = hammerArray(impl.second, nthreads, nwrites, isRTM);
+      int total_recorded = impl.second->total();
+      int total_expected = nthreads * nwrites;
+
+      // i should really be using the std::chrono time conversions but this code is low priority
+      auto avg = run_time / (double)total_recorded;
+
+      int missing = total_recorded - total_expected;
+      std::cout << ImplStr[impl.first] << DELIM;
+      std::cout << num_elements << DELIM;
+      std::cout << (double)run_time * 0.001 << DELIM;
+      std::cout << avg * 1000  << std::endl;
+      if (total_recorded != total_expected) {
+	std::cerr << "ERROR: MISSING WRITES!!!" << std::endl;
+      }
+      // std::cout << "Total = " << total_recorded << "\tExpected = " << total_expected << std::endl;
+      // std::cout << ((total_recorded == total_expected) ? 
+      // 		    "All writes were recorded." : 
+      // 		    ((missing<0) ?
+      // 		     (std::string("There were ") + std::to_string(-missing) + std::string(" missing writes!")) :
+      // 		     (std::string("There were ") + std::to_string(missing) + std::string(" writes that should NOT have occured. This should never happen.")))) << std::endl;
+      delete impl.second;
+    }
   }
 
 
